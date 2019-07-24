8e32a83bdd9a72021a9a20cb82e2e01e53739944
Fudge Jordan <fudge@jordan.net.au>
2019-07-25 01:51:01 +1000

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron jumbotron-fluid" style="color: #FFD700; background: #000">
         <div class="container text-center">
             <h1>
-                <small>Welcome to Young Life Barossa</small>
+                <small>Welcome to Barossa Young Life </small>
                 <br>Trivia Night.
             </h1>
         </div>
